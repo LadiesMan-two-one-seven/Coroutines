e5210ed215c9188d6df96d4d7a06022b9ddc1c4d
LadiesMan-two-one-seven <asanagaevskiyi@list.ru>
2026-05-27 19:23:02 +0300

    Creating GUI

---
+++ utils/src/main/kotlin/callbacks/Display.kt
@@ -0,0 +1,35 @@
+package callbacks
+
+import java.awt.BorderLayout
+import java.awt.Dimension
+import javax.swing.JButton
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.JPanel
+import javax.swing.JScrollPane
+import javax.swing.JTextArea
+
+object Display {
+
+    private var infoArea = JTextArea().apply {
+        isEditable = false
+    }
+
+    private val loadButton = JButton("Load Book")
+    private val timerLabel = JLabel("Time: 00:00")
+    private val topPanel = JPanel(BorderLayout()).apply {
+        add(timerLabel, BorderLayout.WEST)
+        add(loadButton, BorderLayout.EAST)
+    }
+
+    private val mainFrame = JFrame("Book and Author info").apply {
+        layout = BorderLayout()
+        add(topPanel, BorderLayout.NORTH)
+        add(JScrollPane(infoArea), BorderLayout.CENTER)
+        size = Dimension(400, 300)
+    }
+
+    fun show() {
+        mainFrame.isVisible = true
+    }
+}
