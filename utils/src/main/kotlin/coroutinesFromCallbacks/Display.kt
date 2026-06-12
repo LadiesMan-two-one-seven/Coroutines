dac305c4395d5d13d91614d0766f2df4d3442764
LadiesMan-two-one-seven <asanagaevskiyi@list.ru>
2026-06-12 13:35:36 +0300

    Coroutines from callbacks

---
+++ utils/src/main/kotlin/coroutinesFromCallbacks/Display.kt
@@ -0,0 +1,102 @@
+package coroutinesFromCallbacks
+
+import entities.Author
+import entities.Book
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.awt.BorderLayout
+import java.awt.Dimension
+import javax.swing.JButton
+import javax.swing.JFrame
+import javax.swing.JLabel
+import javax.swing.JPanel
+import javax.swing.JScrollPane
+import javax.swing.JTextArea
+import kotlin.concurrent.thread
+import kotlin.coroutines.suspendCoroutine
+
+object Display {
+
+    private val scope = CoroutineScope(Dispatchers.Default)
+
+    private var infoArea = JTextArea().apply {
+        isEditable = false
+    }
+
+    private val loadButton = JButton("Load Book").apply {
+        addActionListener {
+            scope.launch {
+                isEnabled = false
+                infoArea.text = "Loading book information...\n"
+                val book = loadBook()
+                infoArea.append("Book: ${book.title}\nYear: ${book.year}\nGenre: ${book.genre}\n")
+                infoArea.append("Loading author information...\n")
+                val author = loadAuthor(book)
+                infoArea.append("Author: ${author.name}\nBiography: ${author.bio}\n")
+                isEnabled = true
+            }
+        }
+    }
+    private val timerLabel = JLabel("Time: 00:00")
+    private val topPanel = JPanel(BorderLayout()).apply {
+        add(timerLabel, BorderLayout.WEST)
+        add(loadButton, BorderLayout.EAST)
+    }
+
+    private val mainFrame = JFrame("Book and Author info").apply {
+        layout = BorderLayout()
+        add(topPanel, BorderLayout.NORTH)
+        add(JScrollPane(infoArea), BorderLayout.CENTER)
+        size = Dimension(400, 300)
+    }
+
+    fun show() {
+        mainFrame.isVisible = true
+        startTimer()
+    }
+
+    private suspend fun loadBook(): Book {
+        return suspendCoroutine { continuation ->
+            loadBook { book ->
+                continuation.resumeWith(Result.success(book))
+            }
+        }
+
+    }
+
+    private suspend fun loadAuthor(book: Book): Author {
+        return suspendCoroutine { continuation ->
+            loadAuthor(book) { author ->
+                continuation.resumeWith(Result.success(author))
+            }
+        }
+    }
+
+    private fun loadBook(callback: (Book) -> Unit) {
+        thread {
+            Thread.sleep(3000)
+            callback(Book("1984", 1949, "Dystopia"))
+        }
+    }
+
+    private fun loadAuthor(book: Book, callback: (Author) -> Unit) {
+        thread {
+            Thread.sleep(3000)
+            callback(Author("George Orwell", "British writer and journalist"))
+        }
+    }
+
+    private fun startTimer() {
+        thread {
+            var totalSeconds = 0
+            while (true) {
+                val minutes = totalSeconds / 60
+                val seconds = totalSeconds % 60
+                timerLabel.text = String.format("Time: %02d:%02d", minutes, seconds)
+                Thread.sleep(1000)
+                totalSeconds++
+            }
+        }
+    }
+}
